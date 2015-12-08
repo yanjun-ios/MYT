ba143ce63f65b4d47fad276901a425035155d4ab
FarMore <514306470@qq.com>
2015-12-08 17:16:46 +0800

    .

---
+++ MYT/RemindViewController.h
@@ -0,0 +1,14 @@
+//
+//  RemindViewController.h
+//  MYT
+//
+//  Created by YUNRUIMAC on 15/12/8.
+//  Copyright © 2015年 YunRui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RemindViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITableView *tableview;
+
+@end
---
+++ MYT/RemindViewController.m
@@ -0,0 +1,56 @@
+//
+//  RemindViewController.m
+//  MYT
+//
+//  Created by YUNRUIMAC on 15/12/8.
+//  Copyright © 2015年 YunRui. All rights reserved.
+//
+
+#import "RemindViewController.h"
+
+@interface RemindViewController ()
+
+@end
+
+@implementation RemindViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    _tableview.delegate=self;
+    _tableview.dataSource=self;
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 4;
+}
+
+// Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+// Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell=[_tableview dequeueReusableCellWithIdentifier:@"cell"];
+    return cell;
+}
+
+- (NSInteger)numberOfRowsInSection:(NSInteger)section
+{
+    return 2;
+}
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
